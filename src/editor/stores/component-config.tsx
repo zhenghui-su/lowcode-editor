ba2dede19f5ddd7de08e6bf26e23b36143c078bf
苏征辉 <343196323@qq.com>
2024-11-12 20:10:17 +0800

    ✨ feat: 新增组件配置项store

---
+++ src/editor/stores/component-config.tsx
@@ -0,0 +1,56 @@
+import { create } from 'zustand';
+import Container from '../materials/Container';
+import Button from '../materials/Button';
+import Page from '../materials/Page';
+
+export interface ComponentConfig {
+	name: string;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	defaultProps: Record<string, any>;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	component: any;
+}
+// 组件映射配置
+// key: 组件名 value: 组件配置(包括组件实例、默认参数)
+interface State {
+	componentConfig: { [key: string]: ComponentConfig };
+}
+
+interface Action {
+	registerConfig: (name: string, componentConfig: ComponentConfig) => void;
+}
+/**
+ * 组件配置 store
+ */
+export const useComponentConfigStore = create<State & Action>((set) => ({
+	componentConfig: {
+		Container: {
+			name: 'Container',
+			defaultProps: {},
+			component: Container,
+		},
+		Button: {
+			name: 'Button',
+			defaultProps: {
+				type: 'primary',
+				text: '按钮',
+			},
+			component: Button,
+		},
+		Page: {
+			name: 'Page',
+			defaultProps: {},
+			component: Page,
+		},
+	},
+	registerConfig: (name, componentConfig) =>
+		set((state) => {
+			return {
+				...state,
+				componentConfig: {
+					...state.componentConfig,
+					[name]: componentConfig,
+				},
+			};
+		}),
+}));
