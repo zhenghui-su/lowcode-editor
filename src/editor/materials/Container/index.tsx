7b6d3158c906a1deb73dc90d44229810c5a1944f
苏征辉 <343196323@qq.com>
2024-11-12 20:10:56 +0800

    ✨ feat: 新增组件物料-Container

---
+++ src/editor/materials/Container/index.tsx
@@ -0,0 +1,21 @@
+import { CommonComponentProps } from '../../interface';
+import { useMaterialDrop } from '../../hooks/useMaterialDrop';
+/**
+ * @description 容器组件
+ */
+const Container = ({ id, children }: CommonComponentProps) => {
+	const { canDrop, drop } = useMaterialDrop(['Button', 'Container'], id);
+
+	return (
+		<div
+			ref={drop}
+			className={`min-h-[100px] p-[20px] ${
+				canDrop ? 'border-[2px] border-[blue]' : 'border-[1px] border-[#000]'
+			}`}
+		>
+			{children}
+		</div>
+	);
+};
+
+export default Container;
