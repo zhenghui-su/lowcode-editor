9df0e867b160c91420c52df99fda780bd5acbff3
苏征辉 <343196323@qq.com>
2024-11-13 19:46:47 +0800

    ✨ feat: 新增组件自定义id属性

--- src/editor/materials/Button/index.tsx
+++ src/editor/materials/Button/index.tsx
@@ -1,15 +1,16 @@
 import { Button as AntdButton } from 'antd';
-import { ButtonType } from 'antd/es/button';
+import { CommonComponentProps } from '../../interface';
 
-export interface ButtonProps {
-	type: ButtonType;
-	text: string;
-}
 /**
  * @description 按钮组件
  */
-const Button = ({ type, text }: ButtonProps) => {
-	return <AntdButton type={type}>{text}</AntdButton>;
+
+const Button = ({ id, type, text }: CommonComponentProps) => {
+	return (
+		<AntdButton data-component-id={id} type={type}>
+			{text}
+		</AntdButton>
+	);
 };
 
 export default Button;
