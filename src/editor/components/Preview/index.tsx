2077832b296754ebdd18eec7ee470aca4ade9bba
苏征辉 <343196323@qq.com>
2024-11-17 21:42:47 +0800

    ✨ feat: 新增预览区域

---
+++ src/editor/components/Preview/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useComponentConfigStore } from '../../stores/component-config';
+import { Component, useComponentsStore } from '../../stores/components';
+
+/**
+ * @description 编辑区组件-全部预览
+ */
+export function Preview() {
+	const { components } = useComponentsStore();
+	const { componentConfig } = useComponentConfigStore();
+
+	function renderComponents(components: Component[]): React.ReactNode {
+		return components.map((component: Component) => {
+			const config = componentConfig?.[component.name];
+
+			if (!config?.prod) return null;
+
+			return React.createElement(
+				config.prod,
+				{
+					key: component.id,
+					id: component.id,
+					name: component.name,
+					styles: component.styles,
+					...config.defaultProps,
+					...component.props,
+				},
+				renderComponents(component.children || []),
+			);
+		});
+	}
+	return <div>{renderComponents(components)}</div>;
+}
