0f8b0c8f7e4c8e396053c7b346f9bdb78d0992b8
苏征辉 <343196323@qq.com>
2024-11-19 16:06:53 +0800

    ✨ feat: 新增组件事件弹窗和多事件绑定删除功能

--- src/editor/components/Setting/ComponentEvent.tsx
+++ src/editor/components/Setting/ComponentEvent.tsx
@@ -1,25 +1,34 @@
-import { Collapse, CollapseProps, Select } from 'antd';
+import { Button, Collapse, CollapseProps } from 'antd';
 import { useComponentConfigStore } from '../../stores/component-config';
+import type { ComponentEvent } from '../../stores/component-config';
 import { useComponentsStore } from '../../stores/components';
-import { GoToLink } from './actions/GoToLink';
-import { ShowMessage } from './actions/ShowMessage';
+import { useState } from 'react';
+import { ActionModal } from './ActionModal';
+import { ActionConfig } from './actions/actionConfig';
+import { DeleteOutlined } from '@ant-design/icons';
 
 /**
  *
  * @description 组件事件编辑区域
  */
 export function ComponentEvent() {
-	const { curComponentId, curComponent, updateComponentProps } =
-		useComponentsStore();
+	const { curComponent, updateComponentProps } = useComponentsStore();
 	const { componentConfig } = useComponentConfigStore();
+	const [actionModalOpen, setActionModalOpen] = useState(false);
+	const [curEvent, setCurEvent] = useState<ComponentEvent>();
 
 	if (!curComponent) return null;
-	// 当选择某个动作的时候，我们把它保存到 store 里
-	function selectAction(eventName: string, value: string) {
-		if (!curComponentId) return;
-		updateComponentProps(curComponentId, {
-			[eventName]: {
-				type: value,
+
+	function deleteAction(event: ComponentEvent, index: number) {
+		if (!curComponent) return;
+
+		const actions = curComponent.props[event.name]?.actions;
+
+		actions.splice(index, 1);
+
+		updateComponentProps(curComponent.id, {
+			[event.name]: {
+				actions: actions,
 			},
 		});
 	}
@@ -29,37 +38,103 @@ export function ComponentEvent() {
 	).map((event) => {
 		return {
 			key: event.name,
-			label: event.label,
+			label: (
+				<div className='flex justify-between leading-[30px]'>
+					{event.label}
+					<Button
+						type='primary'
+						onClick={(e) => {
+							e.stopPropagation();
+
+							setCurEvent(event);
+							setActionModalOpen(true);
+						}}
+					>
+						添加动作
+					</Button>
+				</div>
+			),
 			children: (
 				<div>
-					<div className='flex items-center'>
-						<div>动作：</div>
-						<Select
-							className='w-[160px]'
-							options={[
-								{ label: '显示提示', value: 'showMessage' },
-								{ label: '跳转链接', value: 'goToLink' },
-							]}
-							onChange={(value) => selectAction(event.name, value)}
-							value={curComponent?.props?.[event.name]?.type}
-						/>
-					</div>
-					{/* 跳转链接 */}
-					{curComponent?.props?.[event.name]?.type === 'goToLink' && (
-						<GoToLink event={event} />
-					)}
-					{/* 显示提示消息 */}
-					{curComponent?.props?.[event.name]?.type === 'showMessage' && (
-						<ShowMessage event={event} />
+					{(curComponent.props[event.name]?.actions || []).map(
+						(item: ActionConfig, index: number) => {
+							return (
+								<div>
+									{item.type === 'goToLink' ? (
+										<div className='border border-[#aaa] m-[10px] p-[10px] relative'>
+											<div className='text-[blue]'>跳转链接</div>
+											<div>{item.url}</div>
+											<div
+												style={{
+													position: 'absolute',
+													top: 10,
+													right: 10,
+													cursor: 'pointer',
+												}}
+												onClick={() => deleteAction(event, index)}
+											>
+												<DeleteOutlined />
+											</div>
+										</div>
+									) : null}
+									{item.type === 'showMessage' ? (
+										<div className='border border-[#aaa] m-[10px] p-[10px] relative'>
+											<div className='text-[blue]'>消息弹窗</div>
+											<div>{item.config.type}</div>
+											<div>{item.config.text}</div>
+											<div
+												style={{
+													position: 'absolute',
+													top: 10,
+													right: 10,
+													cursor: 'pointer',
+												}}
+												onClick={() => deleteAction(event, index)}
+											>
+												<DeleteOutlined />
+											</div>
+										</div>
+									) : null}
+								</div>
+							);
+						},
 					)}
 				</div>
 			),
 		};
 	});
 
+	function handleModalOk(config?: ActionConfig) {
+		if (!config || !curEvent || !curComponent) return;
+
+		updateComponentProps(curComponent.id, {
+			[curEvent.name]: {
+				actions: [
+					...(curComponent.props[curEvent.name]?.actions || []),
+					config,
+				],
+			},
+		});
+
+		setActionModalOpen(false);
+	}
+
 	return (
 		<div className='px-[10px]'>
-			<Collapse className='mb-[10px]' items={items} />
+			<Collapse
+				className='mb-[10px]'
+				items={items}
+				defaultActiveKey={componentConfig[curComponent.name].events?.map(
+					(item) => item.name,
+				)}
+			/>
+			<ActionModal
+				visible={actionModalOpen}
+				handleOk={handleModalOk}
+				handleCancel={() => {
+					setActionModalOpen(false);
+				}}
+			/>
 		</div>
 	);
 }
