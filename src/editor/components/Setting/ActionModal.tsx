b12476e94cff1f56a99b5dacb1b8be631f3d1199
zhenghui-su <343196323@qq.com>
2024-11-21 14:55:28 +0800

    ✨ feat: 事件动作弹窗新增组件方法配置

--- src/editor/components/Setting/ActionModal.tsx
+++ src/editor/components/Setting/ActionModal.tsx
@@ -3,6 +3,10 @@ import { Modal, Segmented } from 'antd';
 import { GoToLink, GoToLinkConfig } from './actions/GoToLink';
 import { ShowMessage, ShowMessageConfig } from './actions/ShowMessage';
 import { CustomJS, CustomJSConfig } from './actions/CusTomJS';
+import {
+	ComponentMethod,
+	ComponentMethodConfig,
+} from './actions/ComponentMethod';
 
 interface ActionModalProps {
 	visible: boolean;
@@ -11,7 +15,11 @@ interface ActionModalProps {
 	handleCancel: () => void;
 }
 
-export type ActionConfig = GoToLinkConfig | ShowMessageConfig | CustomJSConfig;
+export type ActionConfig =
+	| GoToLinkConfig
+	| ShowMessageConfig
+	| CustomJSConfig
+	| ComponentMethodConfig;
 /**
  * 组件事件动作配置弹窗
  */
@@ -22,6 +30,7 @@ export function ActionModal(props: ActionModalProps) {
 		goToLink: '访问链接',
 		showMessage: '消息提示',
 		customJS: '自定义 JS',
+		componentMethod: '组件方法',
 	};
 
 	const [key, setKey] = useState<string>('访问链接');
@@ -48,7 +57,7 @@ export function ActionModal(props: ActionModalProps) {
 					value={key}
 					onChange={setKey}
 					block
-					options={['访问链接', '消息提示', '自定义 JS']}
+					options={['访问链接', '消息提示', '组件方法', '自定义 JS']}
 				/>
 				{key === '访问链接' && (
 					<GoToLink
@@ -63,6 +72,17 @@ export function ActionModal(props: ActionModalProps) {
 					<ShowMessage
 						key='showMessage'
 						value={action?.type === 'showMessage' ? action.config : undefined}
+						onChange={(config) => {
+							setCurConfig(config);
+						}}
+					/>
+				)}
+				{key === '组件方法' && (
+					<ComponentMethod
+						key='componentMethod'
+						value={
+							action?.type === 'componentMethod' ? action.config : undefined
+						}
 						onChange={(config) => {
 							setCurConfig(config);
 						}}
