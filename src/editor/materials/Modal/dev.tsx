7033a628dfa599c5ff6a1ec099e611ca49fa0d6f
zhenghui-su <343196323@qq.com>
2025-01-23 19:19:54 +0800

    ✨feat(editor): 新增河流图物料组件及其基础属性

--- src/editor/materials/Modal/dev.tsx
+++ src/editor/materials/Modal/dev.tsx
@@ -1,62 +1,63 @@
-import { useEffect, useRef } from "react";
-import { useMaterialDrop } from "../../hooks/useMaterialDrop";
-import { CommonComponentProps } from "../../interface";
-import { useDrag } from "react-dnd";
+import { useEffect, useRef } from 'react';
+import { useMaterialDrop } from '../../hooks/useMaterialDrop';
+import { CommonComponentProps } from '../../interface';
+import { useDrag } from 'react-dnd';
 
 /**
  * @description 弹框组件-dev开发状态
  */
 function Modal({ id, name, children, title, styles }: CommonComponentProps) {
-  const { canDrop, drop } = useMaterialDrop(
-    [
-      "Button",
-      "Container",
-      "Table",
-      "Form",
-      "Line",
-      "Bar",
-      "Pie",
-      "Scatter",
-      "Radar",
-      "HeatMap",
-      "Sunburst",
-      "Parallel",
-      "Sankey",
-      "Globe",
-    ],
-    id
-  );
+	const { canDrop, drop } = useMaterialDrop(
+		[
+			'Button',
+			'Container',
+			'Table',
+			'Form',
+			'Line',
+			'Bar',
+			'Pie',
+			'Scatter',
+			'Radar',
+			'HeatMap',
+			'Sunburst',
+			'Parallel',
+			'Sankey',
+			'Globe',
+			'River',
+		],
+		id,
+	);
 
-  const divRef = useRef<HTMLDivElement>(null);
+	const divRef = useRef<HTMLDivElement>(null);
 
-  const [_, drag] = useDrag({
-    type: name,
-    item: {
-      type: name,
-      dragType: "move",
-      id: id,
-    },
-  });
+	const [_, drag] = useDrag({
+		type: name,
+		item: {
+			type: name,
+			dragType: 'move',
+			id: id,
+		},
+	});
 
-  useEffect(() => {
-    // 需要将modal里面的所有drag,drop都进行处理
-    drop(divRef);
-    drag(divRef);
-  }, []);
+	useEffect(() => {
+		// 需要将modal里面的所有drag,drop都进行处理
+		drop(divRef);
+		drag(divRef);
+	}, []);
 
-  return (
-    <div
-      ref={divRef}
-      style={styles}
-      data-component-id={id}
-      className={`min-h-[100px] p-[20px] ${
-        canDrop ? "border-[2px] border-[blue]" : "border-[1px] border-[#000]"
-      }`}
-    >
-      <h4>{title}</h4>
-      <div>{children}</div>
-    </div>
-  );
+	return (
+		<div
+			ref={divRef}
+			style={styles}
+			data-component-id={id}
+			className={`min-h-[100px] p-[20px] ${
+				canDrop ? 'border-[2px] border-[blue]' : 'border-[1px] border-[#000]'
+			}`}
+		>
+			<h4>{title}</h4>
+			<div>{children}</div>
+		</div>
+	);
 }
 
 export default Modal;
--- src/editor/materials/Page/dev.tsx
+++ src/editor/materials/Page/dev.tsx
@@ -1,41 +1,42 @@
-import { CommonComponentProps } from "../../interface";
-import { useMaterialDrop } from "../../hooks/useMaterialDrop";
+import { CommonComponentProps } from '../../interface';
+import { useMaterialDrop } from '../../hooks/useMaterialDrop';
 /**
  *
  * @description 页面组件-dev开发状态
  */
 function Page({ id, children, styles }: CommonComponentProps) {
-  const { canDrop, drop } = useMaterialDrop(
-    [
-      "Button",
-      "Container",
-      "Modal",
-      "Table",
-      "Form",
-      "Line",
-      "Bar",
-      "Pie",
-      "Scatter",
-      "Radar",
-      "HeatMap",
-      "Sunburst",
-      "Parallel",
-      "Sankey",
-      "Globe",
-    ],
-    id
-  );
+	const { canDrop, drop } = useMaterialDrop(
+		[
+			'Button',
+			'Container',
+			'Modal',
+			'Table',
+			'Form',
+			'Line',
+			'Bar',
+			'Pie',
+			'Scatter',
+			'Radar',
+			'HeatMap',
+			'Sunburst',
+			'Parallel',
+			'Sankey',
+			'Globe',
+			'River',
+		],
+		id,
+	);
 
-  return (
-    <div
-      data-component-id={id}
-      ref={drop}
-      className="p-[20px] box-border h-[100%] overflow-y-auto"
-      style={{ ...styles, border: canDrop ? "2px solid blue" : "none" }}
-    >
-      {children}
-    </div>
-  );
+	return (
+		<div
+			data-component-id={id}
+			ref={drop}
+			className='p-[20px] box-border h-[100%] overflow-y-auto'
+			style={{ ...styles, border: canDrop ? '2px solid blue' : 'none' }}
+		>
+			{children}
+		</div>
+	);
 }
 
 export default Page;
