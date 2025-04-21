fd6050fe1fcf77db4f295068e39d5a91539f51b8
zhenghui-su <343196323@qq.com>
2025-04-21 16:14:15 +0800

    🐞 fix: 修复弹性容器超出问题

--- src/editor/materials/FlexContainer/dev.tsx
+++ src/editor/materials/FlexContainer/dev.tsx
@@ -1,83 +1,84 @@
-import { CommonComponentProps } from "../../interface";
-import { useMaterialDrop } from "../../hooks/useMaterialDrop";
-import { useEffect, useRef } from "react";
-import { useDrag } from "react-dnd";
-import { Flex } from "antd";
+import { useEffect, useRef } from 'react';
+import { CommonComponentProps } from '../../interface';
+import { useMaterialDrop } from '../../hooks/useMaterialDrop';
+import { useDrag } from 'react-dnd';
+
 /**
  * @description Flex容器组件-dev开发状态
  */
 const FlexContainer = ({
-  id,
-  name,
-  children,
-  styles,
-  gap,
-  wrap,
-  vertical,
-  justify,
-  align,
+	id,
+	name,
+	children,
+	styles,
+	gap,
+	wrap,
+	vertical,
+	justify,
+	align,
 }: CommonComponentProps) => {
-  const { canDrop, drop } = useMaterialDrop(
-    [
-      "Button",
-      "Container",
-      "Table",
-      "Form",
-      "Line",
-      "Bar",
-      "Pie",
-      "Scatter",
-      "Radar",
-      "HeatMap",
-      "Sunburst",
-      "Parallel",
-      "Sankey",
-      "Globe",
-      "River",
-      "Candlestick",
-      "Funnel",
-      "Pressure",
-    ],
-    id
-  );
+	const { canDrop, drop } = useMaterialDrop(
+		[
+			'Button',
+			'Container',
+			'FlexContainer',
+			'Table',
+			'Form',
+			'Line',
+			'Bar',
+			'Pie',
+			'Scatter',
+			'Radar',
+			'HeatMap',
+			'Sunburst',
+			'Parallel',
+			'Sankey',
+			'Globe',
+			'River',
+			'Candlestick',
+			'Funnel',
+			'Pressure',
+		],
+		id,
+	);
 
-  const divRef = useRef<HTMLDivElement>(null);
+	const divRef = useRef<HTMLDivElement>(null);
 
-  const [_, drag] = useDrag({
-    type: name,
-    item: {
-      type: name,
-      dragType: "move",
-      id: id,
-    },
-  });
+	const [_, drag] = useDrag({
+		type: name,
+		item: {
+			type: name,
+			dragType: 'move',
+			id: id,
+		},
+	});
 
-  useEffect(() => {
-    // 需要将container里面的所有drag,drop都进行处理
-    drop(divRef);
-    drag(divRef);
-  }, []);
+	useEffect(() => {
+		// 需要将container里面的所有drag,drop都进行处理
+		drop(divRef);
+		drag(divRef);
+	}, []);
 
-  return (
-    <div
-      data-component-id={id}
-      ref={divRef}
-      style={styles}
-      className={`min-h-[100px] p-[20px] ${
-        canDrop ? "border-[2px] border-[blue]" : "border-[1px] border-[#000]"
-      }`}
-    >
-      <Flex
-        gap={gap}
-        wrap={wrap}
-        vertical={vertical}
-        justify={justify}
-        align={align}
-      >
-        {children}
-      </Flex>
-    </div>
-  );
+	return (
+		<div
+			data-component-id={id}
+			ref={divRef}
+			className={`min-h-[100px] p-[20px] ${
+				canDrop ? 'border-[2px] border-[blue]' : 'border-[1px] border-[#000]'
+			}`}
+			style={{
+				display: 'flex',
+				flexDirection: vertical ? 'column' : 'row',
+				justifyContent: justify,
+				alignItems: align,
+				gap: gap,
+				flexWrap: wrap ? 'wrap' : 'nowrap',
+				...styles,
+			}}
+		>
+			{children}
+		</div>
+	);
 };
 
 export default FlexContainer;
--- src/editor/materials/FlexContainer/prod.tsx
+++ src/editor/materials/FlexContainer/prod.tsx
@@ -1,31 +1,34 @@
-import { CommonComponentProps } from "../../interface";
-import { Flex } from "antd";
+import { CommonComponentProps } from '../../interface';
 /**
  * @description Flex容器组件-dev开发状态
  */
 const FlexContainer = ({
-  id,
-  children,
-  styles,
-  gap,
-  wrap,
-  vertical,
-  justify,
-  align,
+	id,
+	children,
+	styles,
+	gap,
+	wrap,
+	vertical,
+	justify,
+	align,
 }: CommonComponentProps) => {
-  return (
-    <div data-component-id={id} style={styles} className={`p-[20px]`}>
-      <Flex
-        gap={gap}
-        wrap={wrap}
-        vertical={vertical}
-        justify={justify}
-        align={align}
-      >
-        {children}
-      </Flex>
-    </div>
-  );
+	return (
+		<div
+			data-component-id={id}
+			className={`p-[20px]`}
+			style={{
+				display: 'flex',
+				flexDirection: vertical ? 'column' : 'row',
+				justifyContent: justify,
+				alignItems: align,
+				gap: gap,
+				flexWrap: wrap ? 'wrap' : 'nowrap',
+				...styles,
+			}}
+		>
+			{children}
+		</div>
+	);
 };
 
 export default FlexContainer;
